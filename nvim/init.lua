8c44d25a267ba5ee5d5355f93be811971299b85f
Shovel/Liz <luiz.glerg@gmail.com>
2023-10-24 06:40:59 -0300

    Foda-se o git ignore essa porra e uma merda

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require("shovel.core")
+require("shovel.lazy")
+
---
+++ nvim/lua/shovel/plugins/nvim-tree.lua
@@ -0,0 +1,48 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		-- disable netrw at the very start of your init.lua
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		-- set termguicolors to enable highlight groups
+		vim.opt.termguicolors = true
+
+		-- empty setup using defaults
+		require("nvim-tree").setup()
+
+		-- OR setup with some options
+		require("nvim-tree").setup({
+			sort_by = "case_sensitive",
+			view = {
+				width = 30,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = false,
+			},
+        actions = {
+        open_file = {
+            quit_on_open = true,
+        },
+    },
+		})
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file explorer on current file" }
+		) -- toggle file explorer on current file
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+	end,
+}
+
